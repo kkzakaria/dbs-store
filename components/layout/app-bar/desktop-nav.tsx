6c6fceedc6685c4908f5699c91157377f0a4c964
kkzakaria <kkzakaria@outlook.com>
2026-05-28 21:42:21 +0000

    feat: hide Offres link from desktop navigation

--- components/layout/app-bar/desktop-nav.tsx
+++ components/layout/app-bar/desktop-nav.tsx
@@ -125,12 +125,13 @@ export function DesktopNav({ categories }: DesktopNavProps) {
         <NavItem key={category.id} category={category} allCategories={categories} {...navItemProps} />
       ))}
 
-      <Link
+      {/* TODO: Re-enable when "Offres" feature is ready for launch */}
+      {/* <Link
         href="/offres"
         className="rounded-full px-3 py-2 text-sm font-medium text-red-600 transition-colors hover:bg-muted"
       >
         Offres
-      </Link>
+      </Link> */}
 
       {overflow.length > 0 && (
         <div
